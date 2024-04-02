84947a1c599186f3b0a07f1a6aaa25f06c4b2c04
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2024-04-03 05:44:10 +0700

    integration test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,11 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('should can say hello', async () => {
+    const result = await request(app.getHttpServer())
+      .get('/api/users/hello')
+      .query({ name: 'John' });
+    expect(result.text).toBe(`Hello, John!`);
+  });
 });
